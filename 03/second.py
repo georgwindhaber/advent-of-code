57463f04237dac66d69b7f6a4fef73c0c79baafc
georgwindhaber <georg.windhaber@gmail.com>
2022-12-07 21:16:42 +0100

    Add secondary day 3

---
+++ 03/second.py
@@ -0,0 +1,25 @@
+file = open("input.txt", "r")
+rawLines = file.readlines()
+
+lines = []
+for i in rawLines:
+    lines.append(i.rstrip("\n"))
+
+sum = 0
+
+i = 0
+while i < len(lines):
+    rucksacks = lines[i: i + 3]
+    for character in rucksacks[0]:
+        if (character in rucksacks[1] and character in rucksacks[2]):
+            if (character.islower()):
+                sum = sum + ord(character) - 96
+            else:
+                sum = sum + ord(character) - 38
+            break
+        else:
+            print(character, rucksacks)
+
+    i = i + 3
+
+print(sum, i)
